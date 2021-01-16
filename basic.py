61cd394ee98fb88071a48f852788e93c7cfabd54
Rahul Kumar Gupta <rr916122@gmail.com>
2021-01-16 07:02:58 +0530

    beta version ready

--- basic.py
+++ basic.py
@@ -6,7 +6,44 @@ def sheet_length():
 def city(i):
     return sheet.iloc[i,0]
 def zip_code(i):
-    return sheet.iloc[i,1]
+    k = sheet.iloc[i,1]
+    return str(k)
 def county(i):
     return sheet.iloc[i,2]
+# clmn=["City","Zip Code","County","Median Income","Cost Of Living Index","Median Mortgage To Income Ratio","Owner Occupied Homes","Median Rooms In Home","College Degree","Professional","Population","Average Household Size","Median Age","Male To Female Ratio","Married","Divorced","White","Black","Asian","Hispanic Ethnicity"]
 
+dict1 = {
+        "City":[],
+        "Zip Code":[],
+        "County":[],
+        "Median Income":[],
+        "Cost Of Living Index ($)":[],
+        "Median Mortgage To Income Ratio (%)":[],
+        "Owner Occupied Homes (%)":[],
+        "Median Rooms In Home":[],
+        "College Degree (%)":[],
+        "Professional (%)":[],
+        "Population":[],
+        "Average Household Size":[],
+        "Median Age":[],
+        "Male To Female Ratio (%)":[],
+        "Married (%)":[],
+        "Divorced (%)":[],
+        "White (%)":[],
+        "Black (%)":[],
+        "Asian (%)":[],
+        "Hispanic Ethnicity (%)":[],
+        }
+
+clmn = list(dict1.keys())
+
+def save_cred(output_data):
+    try:
+        df2 = pd.read_csv('zipcode.csv')
+    except:
+        df2 = pd.DataFrame(dict1)
+    
+    df0 = pd.DataFrame([output_data], columns=clmn)
+    df = df2.append(df0,ignore_index=True)    
+    df.to_csv('zipcode.csv',index=False)
+    print('data saved')
--- zipwho.py
+++ zipwho.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import random
-from basic import sheet_length,zip_code,county,city
+from basic import sheet_length,zip_code,county,city,save_cred
 
 chrome_path = which('chromedriver.exe')
 chrome_options = Options()
@@ -27,9 +27,28 @@ for k in range(l+1):
     city_= city(k)
     zip_ = zip_code(k)
     county_ = county(k)
-    
+
+    zip_box = driver.find_element_by_xpath('//*[@id="ui_zip"]')
+    zip_box.send_keys('0')
+    zip_box.send_keys(int(zip_))
+    time.sleep(1)
+    search_btm = driver.find_element_by_xpath('//*[@id="search"]/p[2]/input[2]')
+    search_btm.click()
+    time.sleep(5)
+    output = driver.find_elements_by_xpath('//*[@id="details_table"]/table/tbody/tr')
+    output_data = [city_,zip_,county_]
+
+    for i in range(2,19):
+        xpath = '//*[@id="details_table"]/table/tbody/tr['+str(i) +']/td[2]'
+        text = driver.find_element_by_xpath(xpath).text
+        output_data.append(text)
+    print(output_data)
+    # data saved to csv file.
+    save_cred(output_data)
 
 
+    # clear the zip code box
+    zip_box = driver.find_element_by_xpath('//*[@id="ui_zip"]')
+    zip_box.clear()    
 
-
-# driver.quit()
+driver.quit()
